dcc0cb7d375225ffd8f3c34bd8a8b692a108d390
enylund <eric.nylund@yale.edu>
2014-02-04 10:39:54 -0500

    added tumblr api functionality

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
+// Load the http module to create an http server.
+var http = require('http');
+var tumblr = require('tumblr');
 
+var oauth = {
+  consumer_key: 'SUNpLgKqnJjL8biuJCEA0Iktk7oudjtinZIW4n7d7ueJi9N8ZG',
+  consumer_secret: 'ExLHAr9JtVhaBqXhrtWcMURLMz0o6L00S6gk6OYrPvfDvwF7E6',
+  token: 'SUNpLgKqnJjL8biuJCEA0Iktk7oudjtinZIW4n7d7ueJi9N8ZG',
+  token_secret: 'ExLHAr9JtVhaBqXhrtWcMURLMz0o6L00S6gk6OYrPvfDvwF7E6'
+};
+
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  var params = request.url;
+
+  //Stream our response to back to the browser, starting with the headers
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  //
+  // Now begin streaming the payload, which is an HTML document
+  response.write("<html>");
+  response.write("<body>");
+
+ var user = new tumblr.User(oauth);
+ var tagged = new tumblr.Tagged(oauth);
+
+tagged.search('gif', function(error, response) {
+    if (error) {
+          throw new Error(error);
+    }
+
+  console.log(response.post_url);
+});
+
+response.write("</body>");
+response.write("</html>");
+response.end();
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
